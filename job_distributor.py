3c7ad417925fd84f9580cb2b2fa8954a6ba9e55e
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-05-22 17:43:50 -0400

    wrote Distribute function

--- job_distributor.py
+++ job_distributor.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import numpy as np
+import sys
 
 class JobDistributor:
 
@@ -8,6 +9,8 @@ class JobDistributor:
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
         self.jobs = jobs
+        if self.jobs != []: self.Distribute()
+        self.results=[]
 
     def SendJob(self,child,job):
         '''Sends the object job to the child node'''
@@ -18,5 +21,17 @@ class JobDistributor:
         pass
 
     def Distribute(self,jobs=[]):
-        '''This function does the heavy lifting.  Will send jobs to every child as they become free and track the results.'''
-        pass
+        '''This function does the heavy lifting.  Will send jobs to every child as they become free and track the results. If a list of jobs is supplied here, they will be appended to self.jobs'''
+        first = True
+        if jobs == [] and self.jobs == []:
+            sys.stderr.write("Warning: Distributing 0 jobs")
+        else:
+            self.jobs += jobs
+        while 1:
+            if jobs == []: return self.results
+            if first:
+                for i in range(self.size):
+                    if i != self.rank: self.SendJob(i,self.jobs.pop())
+                first = False
+            for i in range(self.size):
+                if i != self.rank and self.GetResult(i) and self.jobs != []: self.SendJob(i,self.jobs.pop())
