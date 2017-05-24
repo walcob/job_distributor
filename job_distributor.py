566af12d9769e61b661afbbe397f37cf885be85d
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-05-24 16:12:27 -0400

    Changed indentation spacing and minor fixes

--- job_distributor.py
+++ job_distributor.py
@@ -4,34 +4,40 @@ import sys
 
 class JobDistributor:
 
-    def __init__(self,comm,jobs=[]):
-        self.comm = comm
-        self.rank = self.comm.Get_rank()
-        self.size = self.comm.Get_size()
-        self.jobs = jobs
-        if self.jobs != []: self.Distribute()
-        self.results=[]
+  def __init__(self,comm,jobs=np.array([])):
+    self.comm = comm
+    self.rank = self.comm.Get_rank()
+    self.size = self.comm.Get_size()
+    self.jobs = jobs
+    self.results=np.array([])
+    if self.jobs.size > 0: self.Distribute()
 
-    def SendJob(self,child,job):
-        '''Sends the object job to the child node'''
-        pass
+  def SendJob(self,child,job):
+    '''Sends the object job to the child node'''
+    pass
 
-    def GetResult(self,child):
-        '''Receives results from child node'''
-        pass
+  def GetResult(self,child):
+    '''Receives results from child node'''
+    pass
+    self.results = np.append(self.results,result)
 
-    def Distribute(self,jobs=[]):
-        '''This function does the heavy lifting.  Will send jobs to every child as they become free and track the results. If a list of jobs is supplied here, they will be appended to self.jobs'''
-        first = True
-        if jobs == [] and self.jobs == []:
-            sys.stderr.write("Warning: Distributing 0 jobs")
-        else:
-            self.jobs += jobs
-        while 1:
-            if jobs == []: return self.results
-            if first:
-                for i in range(self.size):
-                    if i != self.rank: self.SendJob(i,self.jobs.pop())
-                first = False
-            for i in range(self.size):
-                if i != self.rank and self.GetResult(i) and self.jobs != []: self.SendJob(i,self.jobs.pop())
+  def Distribute(self,jobs=np.array([])):
+    '''This function does the heavy lifting.  Will send jobs to every child as they become free and track the results. If a list of jobs is supplied here, they will be appended to self.jobs'''
+    first = True
+    if jobs.size == 0 and self.jobs.size == 0:
+      sys.stderr.write("Warning: Distributing 0 jobs\n")
+      sys.stderr.flush()
+    else:
+      self.jobs = np.concatenate(self.jobs,jobs)
+    while 1:
+      if self.jobs.size == 0: return self.results
+      if first:
+        for i in range(self.size):
+          if i != self.rank:
+              self.SendJob(i,self.jobs[0])
+              self.jobs = self.jobs[1:]
+        first = False
+      for i in range(self.size):
+        if i != self.rank and self.GetResult(i) and self.jobs.size > 0:
+          self.SendJob(i,self.jobs[0])
+          self.jobs = self.jobs[1:]
